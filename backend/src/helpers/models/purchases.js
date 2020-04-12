73b85c276e514149c088ac7aa8540857dbff8485
alfnori <alfnori@gmail.com>
2020-04-12 19:10:56 -0300

    date fixes on list

--- backend/src/helpers/models/purchases.js
+++ backend/src/helpers/models/purchases.js
@@ -1,12 +1,42 @@
 
+const moment = require('moment');
 const { isEmptyObject } = require('../../utils/checker');
 const { logDatabase } = require('../../utils/logger');
 const {
   parseJSON, sanitize, sanitizerEscape, transpileObject,
 } = require('../../utils/transformer');
 
-
 const mongooseRegex = (str) => ({ $regex: new RegExp(sanitizerEscape(str)), $options: 'ig' });
+const mongooseDatex = (date, startDate = null, endDate = null) => {
+  const startDefault = new Date(1900, 0, 1, 0, 0, 0, 0);
+  const endDefault = new Date(2900, 0, 1, 0, 0, 0, 0);
+  let start = startDefault;
+  let end = endDefault;
+  try {
+    if (startDate && endDate) {
+      const mStart = moment(startDate);
+      const mEnd = moment(endDate);
+      if (mStart.isValid() && mEnd.isValid()) {
+        start = mStart.toDate();
+        end = mEnd.toDate();
+      }
+    } else {
+      const mDate = moment(date);
+      if (mDate.isValid()) {
+        start = mDate.startOf('day').toDate();
+        end = mDate.endOf('day').toDate();
+      }
+    }
+  } catch (err) {
+    start = null;
+    end = null;
+  }
+  if ((start === null || end === null)) {
+    start = startDefault;
+    end = endDefault;
+  }
+  return ({ $gte: start, $lt: end });
+};
 
 const assertFilter = (config) => {
   if (!config || (!config.filter && !config.search)) return {};
@@ -16,12 +46,16 @@ const assertFilter = (config) => {
     filter = {
       code: search,
       cpf: search,
+      date: search,
     };
   } else {
     filter = parseJSON(config.filter, {});
   }
   let resultFilter = {};
   if (filter.code) resultFilter.code = mongooseRegex(filter.code);
+  if (filter.dateStart && filter.dateEnd) {
+    resultFilter.date = mongooseDatex(null, filter.dateStart, filter.dateEnd);
+  } else if (filter.date) resultFilter.date = mongooseDatex(filter.date);
   if (filter.cpf) resultFilter.cpf = mongooseRegex(filter.cpf.replace(/[^\w]/g, ''));
   if (isEmptyObject(resultFilter)) return {};
   if (search) {
@@ -38,7 +72,7 @@ const assertOrder = (config) => {
   const sort = parseJSON(config.sort);
   let resultSort = transpileObject(sort,
     (o, k, v) => {
-      const availableSort = ['name', 'document', 'department', 'employmentStatus'].indexOf(k) >= 0;
+      const availableSort = ['cpf', 'code', 'date', 'value'].indexOf(k) >= 0;
       const availableSortType = ['asc', 'desc', 'ascending', 'descending', 1, -1].indexOf(v) >= 0;
       if (availableSort && availableSortType) {
         return [k, v];
